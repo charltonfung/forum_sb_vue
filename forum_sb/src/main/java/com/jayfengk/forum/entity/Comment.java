5a796b854bd339bed193b789c8270e1979cc7182
charltonfung <b03105041@ntu.edu.tw>
2026-06-29 01:12:51 +0800

    fix: comment顯示時間問題

--- forum_sb/src/main/java/com/jayfengk/forum/entity/Comment.java
+++ forum_sb/src/main/java/com/jayfengk/forum/entity/Comment.java
@@ -1,6 +1,8 @@
 package com.jayfengk.forum.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
@@ -32,6 +34,9 @@ public class Comment implements Serializable {
     @TableLogic
     private LocalDateTime deletedAt;
 
+    @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createdAt;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime updatedAt;
 }
